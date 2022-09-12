f1f1b25ffd6d7098df8814522884b5a1eb221a69
V1sha1-S <76083887+V1sha1-S@users.noreply.github.com>
2022-09-12 12:57:37 +0530

    update

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Banner bannerText="Sextant" />
+                <Banner bannerText="CISCO" />
                 <Exhibit name="I'm an exhibit!"></Exhibit>
                 <Exhibit name="I'm also an exhibit!"></Exhibit>
                 <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
